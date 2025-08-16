43a4b534d44386ce780280b300abf3a2c0cadced
Radoje Božić <radojebozic1966@gmail.com>
2025-08-16 11:48:40 +0200

    Dodavanje Instagram naloga

--- frontend/src/partials/Footer.vue
+++ frontend/src/partials/Footer.vue
@@ -39,6 +39,24 @@
                 </a>
               </li>
 
+             <li>
+              <a href="https://www.linkedin.com/in/radoje-bo%C5%BEi%C4%87-9b199930b/" target="_blank" aria-label="LinkedIn"
+                class="flex justify-center items-center text-purple-500 hover:text-purple-400 transition duration-150 ease-in-out">
+                <svg class="w-8 h-8 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 
+                    2.24 5 5 5h14c2.76 0 5-2.24 
+                    5-5v-14c0-2.76-2.24-5-5-5zm-11 
+                    19h-3v-10h3v10zm-1.5-11.27c-.97 
+                    0-1.75-.79-1.75-1.76s.78-1.75 
+                    1.75-1.75 1.75.78 1.75 1.75-.78 1.76-1.75 
+                    1.76zm13.5 11.27h-3v-5.6c0-1.34-.03-3.07-1.87-3.07-1.87 
+                    0-2.16 1.46-2.16 2.97v5.7h-3v-10h2.88v1.37h.04c.4-.75 
+                    1.37-1.54 2.82-1.54 3.01 
+                    0 3.57 1.98 3.57 4.55v5.62z"/>
+                </svg>
+              </a>
+          </li>
+
               <!-- GitHub -->
               <li class="ml-2">
                 <a href="https://github.com/RadojeBozic/expressajt" target="_blank" aria-label="GitHub"
